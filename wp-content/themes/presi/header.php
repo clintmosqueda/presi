be228498da3a476164ee126d2084b2ee2f70019a
Clint Eric Mosqueda <ericmosqueda1991@gmail.com>
2023-02-13 18:27:18 +0800

    responsive service page

--- wp-content/themes/presi/header.php
+++ wp-content/themes/presi/header.php
@@ -26,16 +26,18 @@
       <div class="header-content">
         <a class="header-logo" href="<?php echo resolve_url(); ?>"><img src="<?php echo resolve_asset_url('/images/logo.png'); ?>" alt=""></a>
         <?php import_part('nav')?>
+
+        <div class="hamburger">
+          <div class="hamburger-menu js-hamburger-menu">
+            <span class="hamburger-dash"></span>
+            <span class="hamburger-dash"></span>
+          </div>
+        </div>
       </div>
     <!-- </div> -->
   </header>
 
-  <div class="hamburger">
-    <div class="hamburger-menu js-hamburger-menu">
-      <span class="hamburger-dash"></span>
-      <span class="hamburger-dash"></span>
-    </div>
-  </div>
+
 
   <?php import_part('mega-menu');?>
 
